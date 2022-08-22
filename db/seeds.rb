96e6574ba28cb2d47675ff9c102a9dc17a8d8a9d
Thomas Lebegue <thomas.lebegue@gmail.com>
2022-08-22 14:56:38 +0200

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Guitar.create(name: "Awesome vintage Fender Stratocaster", description: "Nice 1964 Fender Stratocaster, perfect for any type of music", brand: "Fender", category: "Electric Guitars", location:"Paris" daily_price: 160)
+Guitar.create(name: "Taylor 114ce Walnut", description: "A very pure sound and a very fine material, a perfect body zise with an extraordinary griff.", brand: "Taylor", category: "Acoustic Guitars", location:"London" daily_price: 80)
