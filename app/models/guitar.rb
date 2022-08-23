37a70d7df86a20e66675a60857edd53e92bd3794
Thomas Lebegue <thomas.lebegue@gmail.com>
2022-08-23 12:51:21 +0200

    constants to models and forms

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,8 +1,13 @@
 class Guitar < ApplicationRecord
+  BRANDS =  ["Fender", "Gibson"]
+  CATEGORIES = ["Electric", "Acoustic", "Bass"]
+
   belongs_to :user
   has_many :rentals, dependent: :destroy
   validates :name, :description, :category, :location, :daily_price, presence: true
   validates :name, length: { minimum: 5 }
   validates :description, length: { minimum: 20 }
   validates :description, length: { maximum: 200 }
+  validates :brand, inclusion: { in: BRANDS}
+  validates :category, inclusion: { in: CATEGORIES}
 end
