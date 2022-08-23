66ffa8095a817036efa44d8203e2a7b4b1954f44
Jesica Kincaid <jesica.j.kincaid@gmail.com>
2022-08-23 11:32:37 +0200

    added new rental view

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :rentals, only: %i[new create] do
-    get "accept", to: "rentals#accept"
-    get "decline", to: "rentals#decline"
+  resources :guitars do
+    resources :rentals, only: %i[ new create] do
+      # get "accept", to: "rentals#accept"
+      # get "decline", to: "rentals#decline"
+    end
   end
-
-  resources :guitars
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
