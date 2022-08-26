028482e5c6f1da90ce9ae622470d4c4909ffdbc6
Marion Robert <mrobert148@gmail.com>
2022-08-26 11:57:58 +0200

    fixing searching bar after html conflicts

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -15,4 +15,11 @@ class Guitar < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+    pg_search_scope :search_by_name_and_brand_and_category_and_description_and_address,
+      against: [ :name, :brand, :category, :description, :address ],
+      using: {
+        tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      }
 end
